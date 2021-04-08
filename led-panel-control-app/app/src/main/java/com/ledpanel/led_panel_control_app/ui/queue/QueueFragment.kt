711faf7d6a75e13be83b9ad1db3639cae6fa6144
Roman Shadko <61965857+ro-SH@users.noreply.github.com>
2021-04-08 22:20:14 +0300

    Change Queue UI, add Queue landscape layout

--- led-panel-control-app/app/src/main/java/com/ledpanel/led_panel_control_app/ui/queue/QueueFragment.kt
+++ led-panel-control-app/app/src/main/java/com/ledpanel/led_panel_control_app/ui/queue/QueueFragment.kt
@@ -2,10 +2,8 @@ package com.ledpanel.led_panel_control_app.ui.queue
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.*
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -27,7 +25,7 @@ class QueueFragment : Fragment(), QueueAdapter.OnItemClickListener {
     private lateinit var queueViewModel: QueueViewModel
 
     // Adapter for Recycler View
-    var adapter: QueueAdapter? = null
+    private var adapter: QueueAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,7 +36,18 @@ class QueueFragment : Fragment(), QueueAdapter.OnItemClickListener {
         binding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_queue, container, false)
 
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        showAddDialog()
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -75,11 +84,25 @@ class QueueFragment : Fragment(), QueueAdapter.OnItemClickListener {
             override fun onNothingSelected(parent: AdapterView<*>?) { }
         }
 
-        binding.fragmentQueueAddButton.setOnClickListener { onAddButtonClicked() }
+        binding.fragmentQueueSkipButton.setOnClickListener { onSkipButtonClicked() }
 
         binding.fragmentQueueDisplayButton.setOnClickListener {
             hideKeyboard()
             // TODO: CHECK AND SEND DATA
+        }
+    }
+
+    /**
+     *  Called when the skip button is pressed.
+     *  Delete the item on position 0.
+     */
+    private fun onSkipButtonClicked() {
+        if (queueViewModel.queue.isNotEmpty()) {
+            queueViewModel.removeItemAt(0)
+            adapter?.notifyItemRemoved(0)
+            binding.fragmentQueueRvQueueList.scrollToPosition(0)
+
+            // TODO: Show item on pos 0
         }
     }
 
@@ -93,19 +116,31 @@ class QueueFragment : Fragment(), QueueAdapter.OnItemClickListener {
     }
 
     /**
-     *  Called when the add button is pressed.
-     *  Add new Item to the queue if valid.
+     *  Open Add Dialog and add a new item if valid
      */
-    private fun onAddButtonClicked() {
-        hideKeyboard()
-        val time = if (queueViewModel.type.value == QUEUE)
-            binding.fragmentQueueTime.text.toString()
-        else null
-        val text = binding.fragmentQueueItem.text.toString()
-        if (queueViewModel.addQueueItem(text, time)) {
-            adapter?.notifyDataSetChanged()
-            binding.fragmentQueueTime.setText("")
-            binding.fragmentQueueItem.setText("")
+    private fun showAddDialog() {
+        val dialogView = LayoutInflater.from(context)
+                .inflate(R.layout.add_queue_dialog, null)
+
+        dialogView.findViewById<EditText>(R.id.add_queue_dialog__time)
+                .setVisibility(queueViewModel.type.value == QUEUE)
+        dialogView.findViewById<ImageView>(R.id.add_queue_dialog__time_image)
+                .setVisibility(queueViewModel.type.value == QUEUE)
+
+        val builder = MaterialAlertDialogBuilder(requireContext())
+                .setView(dialogView)
+
+        val alertDialog = builder.show()
+
+        val timeField = if (queueViewModel.type.value == QUEUE) dialogView.findViewById<EditText>(R.id.add_queue_dialog__time) else null
+        val itemField = dialogView.findViewById<EditText>(R.id.add_queue_dialog__item)
+
+        dialogView.findViewById<Button>(R.id.add_queue_dialog__edit_button).setOnClickListener {
+            hideKeyboard()
+            alertDialog.dismiss()
+            if (queueViewModel.addQueueItem(itemField.text.toString(), if (queueViewModel.type.value == QUEUE) timeField?.text.toString() else null)) {
+                adapter?.notifyDataSetChanged()
+            }
         }
     }
 
@@ -115,9 +150,6 @@ class QueueFragment : Fragment(), QueueAdapter.OnItemClickListener {
      */
     private fun setVisibility(position: Int) {
         queueViewModel.setQueueType(position)
-
-        binding.fragmentQueueTime.setVisibility(position == QUEUE)
-        binding.fragmentQueueTimeImage.setVisibility(position == QUEUE)
 
         adapter = QueueAdapter(queueViewModel.type.value!!, queueViewModel.queue, this@QueueFragment)
         binding.fragmentQueueRvQueueList.adapter = adapter
